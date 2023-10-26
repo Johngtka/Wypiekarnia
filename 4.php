0cc6ac4ef1d38ff2e1befee50e2cf3ae02287421
Johngtka <johngtka@gmail.com>
2023-10-26 10:02:28 +0200

    feat: added summary orders panel and refactored styles

--- 4.php
+++ 4.php
@@ -81,16 +81,20 @@ if (!isset($_SESSION['user'])) {
 <head>
   <meta charset="utf-8" />
   <title>Podsumowanie</title>
+
   <meta name="description" content="Zamów swoje ulubione delicje" />
   <meta name="keywords" content="ciasta, torty, i, wypieki, na, każdą, okazję" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
   <link rel="icon" href="./logo.png" sizes="64x64" type="image/png" />
   <link rel="stylesheet" href="style.css" type="text/css" />
   <link rel="stylesheet" href="css1/fontello.css" type="text/css" />
   <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+
   <script src="js/bootstrap.min.js"></script>
   <script src="jquery-3.7.0.min.js"></script>
   <script src="scripts.js"></script>
+
   <!--sekcja czcionek-->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -104,7 +108,7 @@ if (!isset($_SESSION['user'])) {
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600&display=swap" rel="stylesheet" />
-  <!--koniec sekcji czcionek-->
+
   <style type="text/css">
     /* #cart {
       width: 1000px;
@@ -132,17 +136,54 @@ if (!isset($_SESSION['user'])) {
       padding-top: 8%;
     } */
 
-    a:hover {
-      color: #fff;
+
+    .summaryPanel {
+      margin: 5%;
+      padding: 5%;
+      margin-left: auto;
+      margin-right: auto;
+      display: flex;
+      background-color: #fff;
+      border-radius: 10%;
+      border: 5px solid #000;
+      flex-wrap: wrap;
     }
 
-    @media only screen and (max-width:600px) and (max-width:850px) and (max-width:1000px) {
+    .summaryContent {
+      text-align: left;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    @media screen and (max-width:1000px) {
+
+      .summaryPanel {
+        width: 80%;
+        border-radius: 5%;
+      }
+
+    }
+
+    @media screen and (min-width:1000px) {
+
+      .summaryPanel {
+        width: 50%;
+        border-radius: 5%;
+      }
+
+    }
+
+    /* a:hover {
+      color: #fff;
+    } */
+
+    /* @media only screen and (max-width:600px) and (max-width:850px) and (max-width:1000px) {
 
       a {
         width: 100%;
       }
 
-    }
+    } */
   </style>
 </head>
 
@@ -170,17 +211,22 @@ if (!isset($_SESSION['user'])) {
     </ul>
   </div>
   <div class="main">
-    <?php
-    echo "<h1>Podsumowanie</h1>";
-    echo "<p>Zamówiłeś " . $orderData['count'] . " " . $conf . "</p>";
-    echo "<p><b> (" . $prodName . ") </b></p>";
-    echo "<p>Na e-mail: " . $_SESSION['user']['email'] . "<p>";
-    echo "<p>Numer Telefonu: " . $orderData['phone'] . "<p>";
-    echo "<p>Na termin: " . $orderData['date'] . "</p>";
-    echo "<p>Godzinę: " . $orderData['time'] . "</p>";
-    echo "<h1>Z komentarzem:</h1>";
-    echo "<br> " . $orderData['comment'] . "<br><br>";
-    ?>
+    <div class="summaryPanel">
+      <div class="summaryContent">
+        <?php
+        echo "<h1>Podsumowanie</h1>";
+        echo "<p>Zamówiłeś " . $orderData['count'] . " " . $conf . "</p>";
+        echo "<p><b> (" . $prodNameSelected . ") </b></p>";
+        echo "<p>Numer Telefonu: " . $orderData['phone'] . "<p>";
+        echo "<p>Na termin: " . $orderData['date'] . "</p>";
+        echo "<p>Godzinę: " . $orderData['time'] . "</p>";
+        echo "<h1>Z komentarzem:</h1>";
+        echo "<p> " . $orderData['comment'] . "</p>";
+        echo "<p><b> *Po przetworzeniu twojego zamówienia </br> wszystke informację dostaniesz na e-mail: " . $_SESSION['user']['email'] . "</b><p>";
+        ?>
+      </div>
+    </div>
+
     <!-- <div class="pay">
       <i class="icon-credit-card-alt"></i>
       <i class="icon-cc-visa"></i>
